4fcb9967f3e71db659ebdbde60f80356bbfc3c26
AlyanSal <sah240607@gmail.com>
2026-01-10 23:37:38 -0500

    Delete build directory

--- build/CMakeFiles/testing.dir/testing/test.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/testing.dir/testing/test.cpp.obj: \
- C:\Users\sah24\OneDrive\Desktop\University\Projects\Engine\testing\test.cpp \
- C:/Users/sah24/OneDrive/Desktop/University/Projects/Engine/core/memory/arena_allocator.hpp
